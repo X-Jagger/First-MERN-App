c58800d93ba75b87590e7854f904ccea4a0eeb40
Lei Jie <839054164@qq.com>
2017-08-18 22:42:05 +0800

    understand Router v4

--- src/IssueEdit.jsx
+++ src/IssueEdit.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
-import PropTypes from 'prop-types'
+// import PropTypes from 'prop-types'
 export default class IssueEdit extends React.Component {
+	
 	render() {
+		console.log("Eidt!!:",this.props)
 		return (
 			<div> 
-			<p>This is a placeholder for the Issue Eidt page {this.props.params.id}.</p>
+			<p>This is a placeholder for the Issue Eidt page 
+			{this.props.match.params.id}.</p>
 			<Link to="/issues">Back to issue list</Link>
 			</div>
 			)
@@ -15,6 +18,6 @@ export default class IssueEdit extends React.Component {
 // IssueEdit.propTypes = {
 // 	params: React.propTypes.object.isRequired,
 // }
-IssueEdit.propTypes = {
-	params: React.PropTypes.object.isRequired,
-};
+// IssueEdit.propTypes = {
+// 	params: React.PropTypes.object.isRequired,
+// };
