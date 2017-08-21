ec11d91e640f519a6fc819765db21944ca841022
Lei Jie <839054164@qq.com>
2017-08-22 02:00:10 +0800

    add a RegEx for date

--- src/DateInput.jsx
+++ src/DateInput.jsx
@@ -24,6 +24,7 @@ export default class DateInput extends React.Component {
 	}
 	onBlur(e) {
 		const value = this.unformat(this.state.value);
+		// === '' 排除空的日期为null的情况
 		const valid = this.state.value === '' || value != null;
 		if(valid !== this.state.valid && this.props.onValidityChange){
 			this.props.onValidityChange(e,valid)
@@ -34,6 +35,7 @@ export default class DateInput extends React.Component {
 		});
 		if (valid) this.props.onChange(e,value);
 	}
+	//不太合理
 	onChange(e) {
 		if(e.target.value.match(/^[\d-]*$/)) {
 			this.setState({
@@ -51,7 +53,9 @@ export default class DateInput extends React.Component {
 	editFormat(date) {
 		return (date != null) ? date.toISOString().substr(0,10) : '';
 	}
+	//判断valid,如果不符合日期格式,则返回null
 	unformat(str) {
+		str = str.match(/^\d{4}-\d{2}-\d{2}$/) ? str : ''
 		const val = new Date(str);
 		return isNaN(val.getTime()) ? null : val;
 	}
